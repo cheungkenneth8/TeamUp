73a7170340d9f15b567d52069b4a032e7cb19538
dotnet-new <dotnet_new@hotmail.com>
2018-12-16 01:09:11 +0100

    Initial commit

---
+++ TeamUp.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using TeamUp.API.Models;
+
+namespace TeamUp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+
+}
